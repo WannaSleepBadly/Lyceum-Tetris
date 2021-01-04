b3bb2d87fad178d1ae341650c7584c4bc85abc0b
ChocoChip <rapoportjuliaskype@gmail.com>
2021-01-04 17:09:07 +0300

    Окно есть

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Боже упаси от дедлайна 3.0")
+import pygame
+
+
+W, H = 10, 15
+TILE = 45
+GAME_RES = W * TILE, H * TILE  # Игровое разрешение
+
+
+pygame.init()
+game_sc = pygame.display.set_mode(GAME_RES)
+clock = pygame.time.Clock()
+
+while True:
+    game_sc.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    pygame.display.flip()
+    clock.tick()
