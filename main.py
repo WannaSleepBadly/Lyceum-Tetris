284011683e945084b9192fcdefbe6f263381fd40
ChocoChip <rapoportjuliaskype@gmail.com>
2021-01-04 17:14:14 +0300

    Доска

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import pygame
 W, H = 10, 15
 TILE = 45
 GAME_RES = W * TILE, H * TILE  # Игровое разрешение
-
+fps = 60
 
 pygame.init()
 game_sc = pygame.display.set_mode(GAME_RES)
 clock = pygame.time.Clock()
+
+grid = [pygame.Rect(x * TILE, y * TILE, TILE, TILE) for x in range(W) for y in range(H)]  # Доска
 
 while True:
     game_sc.fill((0, 0, 0))
@@ -17,5 +19,6 @@ while True:
         if event.type == pygame.QUIT:
             exit()
 
+    [pygame.draw.rect(game_sc, (255, 255, 255), i_rect, 1) for i_rect in grid]  # Отрисовка доски
     pygame.display.flip()
-    clock.tick()
+    clock.tick(fps)
